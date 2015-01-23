05a564c2817b20fe1ba38754ce749e11d7a5e2bb
Alex Kaufman <alexkaufman06@gmail.com>
2015-01-23 12:29:41 -0800

    Add app.rb, index.erb, and Gemfile.lock files

---
+++ app.rb
@@ -0,0 +1,12 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/stylists")
+require("./lib/clients")
+require("pg")
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get("/") do
+  erb(:index)
+end
