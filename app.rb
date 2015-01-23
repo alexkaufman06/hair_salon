0c3a71f943ae2ab4638e303ee643e632f274a20c
Alex Kaufman <alexkaufman06@gmail.com>
2015-01-23 13:15:21 -0800

    Allow the user to add and delete stylists from the index.erb page

--- app.rb
+++ app.rb
@@ -10,3 +10,15 @@ DB = PG.connect({:dbname => "hair_salon"})
 get("/") do
   erb(:index)
 end
+
+post("/stylists") do
+  stylist_name = params.fetch('stylist_name')
+  Stylist.new({ :name => stylist_name }).save()
+  erb(:index)
+end
+
+delete("/stylists") do
+  stylist_id = params.fetch('stylist_id').to_i()
+  Stylist.find(stylist_id).delete()
+  erb(:index)
+end
